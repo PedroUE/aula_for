01de0c83d23ea8a2ad5d79966a8125fa39c4f258
Pedro U <pedroescapalete@gmail.com>
2025-04-04 11:29:04 -0300

    ex03

--- exercicios_for/ex03.js
+++ exercicios_for/ex03.js
@@ -1,8 +1,8 @@
 for (let i = 1; i <= 99; i++){
     if (i % 2 == 1 ){
-        console.log(i)
+        console.log(i);
     }
 }
 
-//Pedro Urbano 10:52
-//04/04/25
+//Pedro Urbano 10:52;
+//04/04/25;
