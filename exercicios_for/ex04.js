c667f1d5f74954429c93c8865fc25674e42f647f
Pedro U <pedroescapalete@gmail.com>
2025-04-04 11:27:12 -0300

    ex04

--- exercicios_for/ex04.js
+++ exercicios_for/ex04.js
@@ -1,9 +1,9 @@
-let adicao = 0
+let adicao = 0;
 for (let i = 1; i <= 20; i++){
-    adicao += i
-    console.log(`A soma dos numeros é ${adicao}`)
+    adicao += i;
+    console.log(`A soma dos numeros é ${adicao}`);
 }
 
 
-//Pedro Urbano 11:03
-//04/04/25
+//Pedro Urbano 11:03;
+//04/04/25;
